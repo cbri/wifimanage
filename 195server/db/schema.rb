52411e4b6119221c953c141014070639c862a250
chenfw <chenfw0923@163.com>
2014-08-08 16:54:29 +0800

    ok

--- 195server/db/schema.rb
+++ 195server/db/schema.rb
@@ -13,6 +13,127 @@
 
 ActiveRecord::Schema.define(:version => 20140619045607) do
 
+  create_table "access_nodes", :force => true do |t|
+    t.string   "name"
+    t.string   "mac"
+    t.integer  "sys_uptime"
+    t.integer  "sys_upload"
+    t.integer  "sys_memfree"
+    t.string   "remote_addr"
+    t.integer  "update_time"
+    t.datetime "last_seen"
+    t.string   "redirect_url"
+    t.string   "portal_url"
+    t.datetime "created_at",                        :null => false
+    t.datetime "updated_at",                        :null => false
+    t.boolean  "configflag",   :default => false
+    t.string   "cmdline"
+    t.integer  "time_limit"
+    t.float    "lat",          :default => 32.0266
+    t.float    "long",         :default => 118.788
+    t.string   "developer"
+    t.boolean  "cmdflag"
+  end
+
+  add_index "access_nodes", ["mac"], :name => "index_access_nodes_on_mac", :unique => true
+
+  create_table "access_nods", :force => true do |t|
+    t.string   "name"
+    t.string   "mac"
+    t.string   "redirect_url"
+    t.string   "portal_url"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
+
+  create_table "admins", :force => true do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "token"
+  end
+
+  create_table "auths", :force => true do |t|
+    t.string   "auth_type",      :default => "radius"
+    t.boolean  "auth_device",    :default => false
+    t.datetime "created_at",                           :null => false
+    t.datetime "updated_at",                           :null => false
+    t.integer  "access_node_id"
+  end
+
+  create_table "authservers", :force => true do |t|
+    t.string   "ipaddr"
+    t.integer  "access_node_id"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+    t.string   "node_mac"
+  end
+
+  create_table "black_macs", :force => true do |t|
+    t.integer  "access_node_id"
+    t.string   "mac"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+
+  create_table "confs", :force => true do |t|
+    t.integer  "checkinterval",  :default => 60
+    t.integer  "authinterval",   :default => 120
+    t.integer  "clienttimeout",  :default => 5
+    t.integer  "httpmaxconn",    :default => 10
+    t.integer  "access_node_id"
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
+    t.string   "authserver"
+  end
+
+  create_table "connections", :force => true do |t|
+    t.string   "token"
+    t.string   "ipaddr"
+    t.string   "access_mac"
+    t.datetime "used_on"
+    t.datetime "expired_on"
+    t.string   "mac"
+    t.integer  "incoming",       :default => 0
+    t.integer  "outgoing",       :default => 0
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+    t.integer  "access_node_id"
+    t.string   "device"
+    t.string   "portal_url"
+    t.string   "phonenum"
+  end
+
+  create_table "guests", :force => true do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.string   "password_token"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
+
+  create_table "public_ips", :force => true do |t|
+    t.integer  "access_node_id"
+    t.string   "publicip"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+
+  create_table "statuses", :force => true do |t|
+    t.integer  "code"
+    t.string   "message"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "trusted_macs", :force => true do |t|
+    t.integer  "access_node_id"
+    t.string   "mac"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+  
   create_table "servers", :force => true do |t|
     t.string  "server_ip"
     t.string   "province_code"
